bf687b972e96fd02d7b9bbe059d4c1074b346b5c
Clyde <lkwowfs@163.com>
2019-06-06 13:34:05 +0800

    初始化

---
+++ src/main/java/com/uzone/settlement/SpringcloudStatementApplication.java
@@ -0,0 +1,13 @@
+package com.uzone.settlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudStatementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudStatementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uzone/settlement/SpringcloudStatementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uzone.settlement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudStatementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
